73118d3bb5e1cc8f60b89c2d57ea8fa243725e40
sedseuron <sed.seuron@gmail.com>
2021-11-05 11:08:36 +0100

    Added express and initial endpoints to index.js

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-console.log("Dune API");
+const express = require('express');
+const app = express();
+const port = 3030;
+
+app.get('/documentation', (req, res) => {
+    res.send('Bienvenido a la documentación de DUNE API');
+
+});
+
+app.get('/about', (req, res) => {
+    res.send('About the web and me');
+
+});
+
+app.get('/login', (req, res) => {
+    res.send('Login a Dune API REST');
+
+});
+
+app.get('/characters', (req, res) => {
+    res.json({
+        id_name: 1,
+        name: 'Paul Atreides',
+        faction: 'Atreides',
+        planet: 'Caladan'
+    });
+
+});
+
+app.get('/factions', (req, res) => {
+    res.json({
+        id_faction: 1,
+        faction: 'Atreides'
+    });
+
+});
+
+app.get('/Planets', (req, res) => {
+    res.json({
+        id_planet: 1,
+        name: 'Caladan'
+
+    });
+
+});
+
+
+app.listen(port, () => {
+    console.log('Mi puerto ' + port);
+});
