eff0536d0de7c217436a9421f6ca05450ffc6364
sedseuron <sed.seuron@gmail.com>
2021-11-22 11:49:46 +0100

    cors and pre-deploy

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express');
+const cors = require('cors')
 const routerApi = require('./routes');
 const path = require('path');
 
 const {  logErrors, errorHandler, boomErrorHandler  } = require("./middlewares/errorHandler");
 
 const app = express();
-const port = 3030;
+const port = process.env.PORT || 3030;
 
-app.use(express.json())
+app.use(express.json());
+const whitelist = [''];
+const options = {
+    origin: (origin, callback) => {
+        if (whitelist.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('No permitido'));
+        }
+    }
+}
+app.use(cors(options));
 
 app.get('/', (req, res) => {
     res.status(200).json({
